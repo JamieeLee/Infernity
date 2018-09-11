d3b87aca2289ec9942f87213db9fc53fa02fe6a6
qndel <stefan551@o2.pl>
2018-09-11 12:42:41 +0200

    added automatically creating a .zip archive with infernity files

---
+++ cmd.bat
@@ -0,0 +1,5 @@
+E:
+cd E:\MY_D1_MOD\Infernity\
+copy E:\MY_D1_MOD\Infernity\lootFilter.lua E:\MY_D1_MOD\Infernity\Source\DiabloGame\lootFilter.lua
+"C:\Program Files\7-Zip\7z.exe" a -tzip Infernity.zip @zipfiles.txt
+start 
