df3dbbed2fc36c4bd83e1b7a63f4cbec7cd98113
qndel <stefan551@o2.pl>
2019-08-09 10:50:44 +0200

    version 1.22 - multiplayer fixed, stash reduced from 100 tabs to 25, items in tabs above 25 will be lost

--- cmd.bat
+++ cmd.bat
@@ -2,4 +2,5 @@ G:
 cd G:\LATEST_STUFF\MY_D1_MOD\Infernity\
 copy G:\LATEST_STUFF\MY_D1_MOD\Infernity\lootFilter.lua G:\LATEST_STUFF\MY_D1_MOD\Infernity\Source\DiabloGame\lootFilter.lua
 "C:\Program Files\7-Zip\7z.exe" a -tzip Infernity.zip @zipfiles.txt
+pause
 start 
